ed0bccf083474a6bcd258854106f2ae1b8769d1f
duhee <duhee.lee@influential.co.kr>
2022-03-11 16:49:45 +0900

    define user ORM class

--- blog_control/user_mgmt.py
+++ blog_control/user_mgmt.py
@@ -1,1 +1,22 @@
-# user 를 객체로 정의하고 db 의 user table 에 접근/수정하는 코드 구현
+# user 를 객체로 정의
+from flask_login import UserMixin
+from sqlalchemy import create_engine, Column, Integer, String
+engine = create_engine( "mysql+pymysql://root:@localhost:3306/blog_db", echo=True)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+
